3dd46def8f76e4bdff3ad98c69ceab61cebe1132
SungjinJo <sp9448@naver.com>
2021-05-20 22:36:23 +0900

    Level 1 - Swift : 서울에서 김서방 찾기

---
+++ Programmers/Level 1/Swift/서울에서 김서방 찾기.swift
@@ -0,0 +1,10 @@
+//
+//  서울에서 김서방 찾기.swift
+//  
+//
+//  Created by Sungjin Jo on 2021/05/20.
+//
+
+func solution(_ seoul:[String]) -> String {
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+}
