855cf670e5e9617552dc48b0f15f31a5583de057
SungjinJo <sp9448@naver.com>
2021-05-24 11:27:26 +0900

    Level 1 - Swift : 핸드폰 번호 가리기

---
+++ Programmers/Level 1/Swift/핸드폰 번호 가리기.swift
@@ -0,0 +1,13 @@
+//
+//  핸드폰 번호 가리기.swift
+//  
+//
+//  Created by Sungjin Jo on 2021/05/24.
+//
+
+func solution(_ phone_number:String) -> String {
+    // sufix : 뒤에서 ~자리만 뽑아옴 <-> prefix
+    return String(repeating: "*", count: phone_number.count - 4) + phone_number.suffix(4)
+    // 또는 아래와 같이 작성 가능
+    //return String("\(String(repeating: "*", count: phone_number.count - 4))\(phone_number.suffix(4)))")
+}
