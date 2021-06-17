3fbb64ac1a66f0580b4ee88ebd3532ccbb1045c4
SungjinJo <sp9448@naver.com>
2021-06-18 01:10:03 +0900

    Level 1 - Swift : 정수 제곱근 판별

--- Programmers/Level 1/Swift/정수 제곱근 판별.swift
+++ Programmers/Level 1/Swift/정수 제곱근 판별.swift
@@ -10,6 +10,6 @@ import Foundation
 func solution(_ n:Int64) -> Int64 {
     let ans = Int64(sqrt(Double(n)))
     // return n == Int64(pow(Double(ans), 2)) ? Int64(pow(Double(ans) + 1, 2)) : -1
-    // 간단하게 정리하면
+    // 간단하게 정리해보면
     return n == ans * ans ? (ans + 1) * (ans + 1) : -1
 }
